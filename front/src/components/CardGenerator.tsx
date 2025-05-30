2fe729042ed10d8a91dea125b7dfffb43425c628
Thomas Riffard <ferdodo@hotmail.fr>
2025-05-30 14:10:14 +0200

    carg generator setup

--- front/src/components/CardGenerator.tsx
+++ front/src/components/CardGenerator.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { GeneratePlayerCardUseCase } from "core";
+import type { PlayerCard } from "core";
+import React, { useState } from "react";
 import { useNavigation } from "../hooks/useNavigation";
 
 export function CardGenerator() {
 	const { goToMainMenu } = useNavigation();
+	const [playerCard, setPlayerCard] = useState<PlayerCard | null>(null);
+
+	async function generateCard() {
+		const value = await new GeneratePlayerCardUseCase().execute();
+		setPlayerCard(value);
+	}
 
 	return (
 		<div>
@@ -10,6 +18,11 @@ export function CardGenerator() {
 			<button type="button" onClick={goToMainMenu}>
 				Retour
 			</button>
+			<button type="button" onClick={generateCard}>
+				Generate Card
+			</button>
+
+			{playerCard && JSON.stringify(playerCard)}
 		</div>
 	);
 }
